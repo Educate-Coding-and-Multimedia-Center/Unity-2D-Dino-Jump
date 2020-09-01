24f6a7a4df0b65e724b130acad8636f34edfcb7c
E-ducate Pontianak <storage.educate@gmail.com>
2020-09-01 11:10:03 +0700

    Add Dino Jump Project

---
+++ Assets/Scripts/Dino.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dino : MonoBehaviour {
+
+	public float jumpPower = 5f;
+	public float moveSpeed = 10f;
+	Rigidbody2D body;
+
+	// Use this for initialization
+	void Start () {
+		body = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey(KeyCode.Space)) {
+			Vector2 force = new Vector2(0, jumpPower);
+			body.velocity = force;
+		}
+		if (Input.GetKey(KeyCode.A)) {
+			Vector2 force = new Vector2(-moveSpeed, body.velocity.y);
+			body.velocity = force;
+		}
+		if (Input.GetKey(KeyCode.D)) {
+			Vector2 force = new Vector2(moveSpeed, body.velocity.y);
+			body.velocity = force;
+		}
+	}
+}
